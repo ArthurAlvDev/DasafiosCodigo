7ac89f02b6dba3fa5bcd8653d2954db6b640e49c
ArthurAlvDev <69433870+ArthurAlvDev@users.noreply.github.com>
2024-09-30 23:41:29 -0300

    Create Desafio01.java

---
+++ Java/Desafio01.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class JogoAdivinhacao {
+    public static void main(String[] args) {
+        
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        
+        int numeroSecreto = random.nextInt(100) + 1;
+        int tentativas = 0;
+        int chute = 0;
+        
+        System.out.println("Bem vindo ao jogo de adivinhação!");
+        System.out.println("Tente adivinhar o número entre 1 e 100.");
+        
+        while (chute != numeroSecreto) {
+            System.out.print("Qual o seu palpite?");
+            chute = scanner.nextInt();
+            tentativas++;
+            
+            if (chute < numeroSecreto) {
+                System.out.println("Muito baixo. Tente novamente.");
+            }else if (chute > numeroSecreto){
+                System.out.println("Muito alto. Tente novamente.");
+            }else {
+                System.out.println("Parabéns! Você acertou o número em " + tentativas + " tentativas.");
+            }
+        }
+        scanner.close();
+    }
+}
